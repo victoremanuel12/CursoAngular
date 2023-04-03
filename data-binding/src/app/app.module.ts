899d1f0053616c910e75d97a64dc271559b27996
Victor Emanuel <victoremannuel12@hotmail.com>
2023-04-03 14:39:31 -0300

    curso angular

---
+++ data-binding/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataBidingComponent } from './data-biding/data-biding.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBidingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
